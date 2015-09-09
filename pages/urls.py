16f0c7bbace89f93e7cf0fa243d951940bb610e9
JB <juliano.binder@gmail.com>
2015-09-09 16:45:41 +0100

    fix the relative_url

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
+from django.conf.urls import include, url
+from django.contrib import admin
+
 from .views import PageView
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<relative_url>.*)$', PageView.as_view(), name='page'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
+import pdb
+
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.views.generic.base import View
@@ -8,10 +10,13 @@ from .models import Page
 class PageView(View):
     
     def get(self, request, relative_url):
-        
+
+        relative_url = relative_url.split('/')[-1]
+        #pdb.set_trace()
         if not Page.objects.filter(relative_url=relative_url).exists():
             return render_to_response('page-does-not-exist.html')
 
+        
         page = Page.objects.get(relative_url=relative_url)
         page.access_count += 1
         page.save()
