d5b5e0d90c0008849aa850f1a61ecda12459acda
JB <juliano.binder@gmail.com>
2015-09-30 17:27:54 +0100

    pages admin

--- pages/admin.py
+++ pages/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Page, MetaTag
 
-admin.site.register(Page)
-admin.site.register(MetaTag)
+class PageAdmin(admin.ModelAdmin):
+    list_filter = ('title','relative_url','template','access_count')
+    list_display = ('title','relative_url','template','access_count')
+    search_fields = ['title','relative_url','template','access_count']
+
+admin.site.register(Page, PageAdmin)
+
+class MetaTagAdmin(admin.ModelAdmin):
+    list_filter = ('page','name','property','content')
+    list_display = ('page','name','property','content')
+    search_fields = ['name','property','content']
+    
+admin.site.register(MetaTag, MetaTagAdmin)
