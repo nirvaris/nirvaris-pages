fe8ae795b9fab863e71360f709352fce64119eb3
JB <juliano.binder@gmail.com>
2017-11-07 17:14:33 +0000

    django update

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 import pdb
 
-from django.shortcuts import render, render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 from django.views.generic.base import View
 # Create your views here.
 
@@ -14,12 +13,12 @@ class PageView(View):
         relative_url = relative_url.split('/')[-1]
         #pdb.set_trace()
         if not Page.objects.filter(relative_url=relative_url).exists():
-            return render_to_response('page-does-not-exist.html')
+            return render(request,'page-does-not-exist.html')
 
         page = Page.objects.get(relative_url=relative_url)
         page.access_count += 1
         page.save()
-        context_variables = {
+        data_context = {
             'page': page,
             'title': page.title,
         }
@@ -43,8 +42,6 @@ class PageView(View):
                     'content': meta_tag.content,
                 })
 
-        context_variables['meta_data_locals'] = meta_data_locals
+        data_context['meta_data_locals'] = meta_data_locals
 
-        request_context = RequestContext(request, context_variables)
-
-        return render_to_response(page.template, request_context)
+        return render(request,page.template, data_context)
